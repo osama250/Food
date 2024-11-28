1e57e0e5d3335509c8a6837381cb9497f8b1518a
osama yousry <osama250>
2024-11-28 16:33:35 +0200

    28_11_2024

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Contact;
 use App\Models\Setting;
-
+use App\Models\Rice;
+use App\Models\Bread;
+use App\Models\Drink;
+use App\Models\Salad;
 
 class SettingController extends Controller
 {
@@ -15,12 +18,20 @@ class SettingController extends Controller
         $settings       =  Setting::get()->first();
         $categories     =  Category::get();
         $contacts       =  Contact::get()->first();
+        $ricee          =  Rice::get();
+        $braeds         =  Bread::get();
+        $drinks         =  Drink::get();
+        $salads         =  Salad::get();
 
         return response()->json([
             'status'        => 'true' ,
             'settings'      => $settings ,
             'categories'    => $categories ,
-            'contacts'      => $contacts
+            'contacts'      => $contacts ,
+            'ricee'         => $ricee ,
+            'braeds'        => $braeds ,
+            'drinks'        => $drinks ,
+            'salads'        => $salads
         ] , 200 );
     }
 }
